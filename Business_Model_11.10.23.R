215cb5cd071717ee6dbf3b1ad336655b3ede9d8d
slkelly22 <slkelly@go.olemiss.edu>
2023-11-10 12:53:37 -0600

    added options(scipen=999)

--- Business_Model_11.10.23.R
+++ Business_Model_11.10.23.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(readxl)
 library(car) #for vif analysis
+options(scipen = 999)
 
 #importing the dataset with the raw and z variables (created the z-scores in R last fall)
 Z_and_Final_Variables_10.28.22 <- read.csv("~/Desktop/R Directory/ResiliencyProject/Z_and_Final_Variables_10.28.22.csv")
