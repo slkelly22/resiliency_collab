5fdc11d951646457a9d3b5fcbebd2ea3d0c21ac8
slkelly22 <slkelly@go.olemiss.edu>
2023-12-11 13:31:32 -0600

    Business Model; Added 2nd DV: z_unemp_perc

--- Business_Model_11.10.23.R
+++ Business_Model_11.10.23.R
@@ -141,3 +141,122 @@ confint(z_gi_levels_12345678)
 
 ## VIF for Final Model
 vif(z_gi_levels_12345678)
+
+#12.11.23
+#Business Model - Adding 2nd DV: z_unemp_perc
+
+# Business Model: Unemp Perc (DV)
+
+## IV: Demographics (9 variables), DV: Unemp Perc
+z_up_just_demos <- lm(formula = z_unemp_perc ~ z_ACS_PCT_BLACK + z_ACS_PCT_HISPAN + z_SVI_RPL_THEME1_SOCIECO + z_SVI_RPL_THEME2_HH_DISB + z_SVI_RPL_THEME3_MINO + z_SVI_RPL_THEME4_HH_TRANS + z_ACS_PCT_ENGL_NOT_WELL + z_ACS_PCT_NON_CITIZEN + z_ACS_MEDIAN_AGE, data = Z_and_Final_Variables_10.28.22) 
+
+summary(z_up_just_demos)
+
+## Confidence Intervals for Demographic IVs
+confint(z_up_just_demos)
+
+## VIF for Demographic IVs
+vif(z_up_just_demos)
+
+## IV: Food & Housing (14 variables), DV: Unemp Perc
+z_up_just_foodhouse <- lm(formula = z_unemp_perc ~ z_food_afford + z_fd_enviro_indx + z_fi_percent + z_ACS_PCT_FOOD_STAMP + z_childelig_lunch_perc + z_shp_percent + z_shcb_percent + z_ACS_PCT_NO_VEH + z_ACS_PCT_RENTER_HH_CHILD + z_ACS_PCT_RENT_COST_30PCT + z_ACS_PCT_MOBILE_HOME + z_ACS_PCT_OWNED_HH + z_ACS_PCT_OWNER_HH_CHILD + z_ACS_MEDIAN_HOME_VALUE, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_foodhouse)
+
+## Confidence Intervals for Food & Housing IVs
+confint(z_up_just_foodhouse)
+
+## VIF for Food & Housing IVs
+vif(z_up_just_foodhouse)
+
+## IV: Education (3 variables), DV: Unemp Perc
+z_up_just_ed <- lm(formula = z_unemp_perc ~ z_hsg_rate + z_sc_percent + z_ms_average, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_ed)
+
+## Confidence Intervals for Education IVs
+confint(z_up_just_ed)
+
+## VIF for Education IVs
+vif(z_up_just_ed)
+
+## IV: Technology (3 variables), DV: Unemp Perc
+z_up_just_tech <- lm(formula = z_unemp_perc ~ z_ACS_PCT_NO_PC + z_ACS_PCT_SMARTPHONE + z_ACS_PCT_BROADBAND, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_tech)
+
+## Confidence Intervals for Technology IVs
+confint(z_up_just_tech)
+
+## VIF for Technology IVs
+vif(z_up_just_tech)
+
+## IV: HWCC (7 variables), DV: Unemp Perc
+z_up_just_HWCC <- lm(formula = z_unemp_perc ~ z_pphd_avgnum + z_pmhd_avgnum + z_lbw_percent_lbw + z_pi_phy_inact + z_cip_perc + z_csph_perc + z_ACS_PCT_CHILD_DISAB, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_HWCC)
+
+## Confidence Intervals for HWCC IVs
+confint(z_up_just_HWCC)
+
+## VIF for HWCC IVs
+vif(z_up_just_HWCC)
+
+## IV: Health A (6 variables), DV: Unemp Perc
+z_up_just_healthA <- lm(formula = z_unemp_perc ~ z_phys_per_cap + z_mhp_per_cap + z_dents_per_cap + z_uninsur_perc + z_opcp_rate + z_TeenBirths, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_healthA)
+
+## Confidence Intervals for Health A IVs
+confint(z_up_just_healthA)
+
+## VIF for Health A IVs
+vif(z_up_just_healthA)
+
+## IV: Health B (5 variables), DV: Unemp Perc
+z_up_just_healthB <- lm(formula = z_unemp_perc ~ z_phs_rate + z_ms_perc_ann + z_AMFAR_AMATFAC + z_MP_PERCPEN + z_AHRF_RURAL_H_CLINIC, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_healthB)
+
+## Confidence Intervals for Health B IVs
+confint(z_up_just_healthB)
+
+## VIF for Health B IVs
+vif(z_up_just_healthB)
+
+## IV: Business (2 variables), DV: Unemp Perc
+## NOTE: both DVs for Business (Gini and unemp_perc) have been removed as IVs here and in the final Business model
+z_up_just_bus <- lm(formula = z_unemp_perc ~ z_ACS_MEDIAN_HH_INCOME + z_ACS_PCT_WORK_NO_CAR, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_bus)
+
+## Confidence Intervals for Business IVs
+confint(z_up_just_bus)
+
+## VIF for Business IVs
+vif(z_up_just_bus)
+
+## IV: Government (5 variables), DV: Unemp Perc
+z_up_just_gov <- lm(formula = z_unemp_perc ~ z_soc_assoc_num + z_aeo_perc_w_acc + z_dwv_presence + z_ACS_PCT_HH_PUB_ASSIST + z_AHRF_TXC_SITE_NO_CNTRL, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_just_gov)
+
+## Confidence Intervals for Government IVs
+confint(z_up_just_gov)
+
+## VIF for Government IVs
+vif(z_up_just_gov)
+
+# Final Government Model, DV: Gini Index; IVs: 54
+
+z_up_levels_12345678 <- lm(formula = z_unemp_perc ~ z_ACS_PCT_BLACK + z_ACS_PCT_HISPAN + z_SVI_RPL_THEME1_SOCIECO + z_SVI_RPL_THEME2_HH_DISB + z_SVI_RPL_THEME3_MINO + z_SVI_RPL_THEME4_HH_TRANS + z_ACS_PCT_ENGL_NOT_WELL + z_ACS_PCT_NON_CITIZEN + z_ACS_MEDIAN_AGE + z_food_afford + z_fd_enviro_indx + z_fi_percent + z_ACS_PCT_FOOD_STAMP + z_childelig_lunch_perc + z_shp_percent + z_shcb_percent + z_ACS_PCT_NO_VEH + z_ACS_PCT_RENTER_HH_CHILD + z_ACS_PCT_RENT_COST_30PCT + z_ACS_PCT_MOBILE_HOME + z_ACS_PCT_OWNED_HH + z_ACS_PCT_OWNER_HH_CHILD + z_ACS_MEDIAN_HOME_VALUE + z_hsg_rate + z_sc_percent + z_ms_average + z_ACS_PCT_NO_PC + z_ACS_PCT_SMARTPHONE + z_ACS_PCT_BROADBAND + z_pphd_avgnum + z_pmhd_avgnum + z_lbw_percent_lbw + z_pi_phy_inact + z_cip_perc + z_csph_perc + z_ACS_PCT_CHILD_DISAB + z_phys_per_cap + z_mhp_per_cap + z_dents_per_cap + z_uninsur_perc + z_opcp_rate + z_TeenBirths + z_phs_rate + z_ms_perc_ann + z_AMFAR_AMATFAC + z_MP_PERCPEN + z_AHRF_RURAL_H_CLINIC + z_ACS_MEDIAN_HH_INCOME + z_ACS_PCT_WORK_NO_CAR + z_soc_assoc_num + z_aeo_perc_w_acc + z_dwv_presence + z_ACS_PCT_HH_PUB_ASSIST + z_AHRF_TXC_SITE_NO_CNTRL, data = Z_and_Final_Variables_10.28.22)
+
+summary(z_up_levels_12345678)
+
+## Confidence Intervals for Final Model
+confint(z_up_levels_12345678)
+
+## VIF for Final Model
+vif(z_up_levels_12345678)
+
+
